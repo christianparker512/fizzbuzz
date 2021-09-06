26e0007a15341d0a3058328fec2570de1751ef34
Christian Parker <christianparker512@gmail.com>
2021-09-05 19:44:11 -0500

    Learning java fizz buzz like"

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -7,8 +7,14 @@ public class Main {
         int sum = 0;
         for (int i = 1; i <=1000; i++ ){
             if ((i % 3 == 0) && (i % 5 == 0)){
-
+                count++;
+                sum += i;
+                System.out.println("Found number = " + i);
+            }
+            if (count == 5){
+                break;
             }
         }
+        System.out.println("The sum = " + sum);
     }
 }
